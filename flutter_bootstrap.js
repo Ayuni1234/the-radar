f28e4f3c588e7986d44d4ac3de6799f672b955f0
Ayuni1234 <Ayuni1234@users.noreply.github.com>
2026-09-19 18:41:34 +0000

    deploy: da33a5e2c41b0ed00208d36fc592ae3a477847de

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3351640058" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3504426863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
